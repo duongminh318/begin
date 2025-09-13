f3e7580b19ac1b2c19cd5463cea384b04eccac55
Win <duongminh318@gmail.com>
2025-09-13 11:46:31 +0700

    lesson11: lấy thông tin từ cái mình nhập vào ô tìm kiém

--- qlsv/src/components/Search.js
+++ qlsv/src/components/Search.js
@@ -1,15 +1,33 @@
 import React, { Component } from 'react';
+import { useDispatch } from 'react-redux';
+import { useState } from "react";
 
-class Search extends Component {
-    render() {
-        return (
-            <form action="list.html" method="GET">
-                <label className="form-inline justify-content-end">Tìm kiếm: <input type="search" name="search" className="form-control" />
-                    <button className="btn btn-danger">Tìm</button>
-                </label>
-            </form>
-        );
+
+const Search = () => {
+    const [search, setSearch] = useState("");
+    const dispath = useDispatch();
+    function handleSearch() {
+        console.log(search);
+        dispath({ type: "search", payload: search });
     }
+    return (
+        <form action="list.html" method="GET">
+            <label className="form-inline justify-content-end">
+                Tìm kiếm:
+                <input type="search"
+                    name="search"
+                    className="form-control"
+                    onChange={(e) => { setSearch(e.target.value) }}
+                />
+                <button className="btn btn-danger"
+                    onClick={(event) => handleSearch()}
+                    type='button'
+                >
+                    Tìm
+                </button>
+            </label>
+        </form>
+    )
 }
 
 export default Search;
