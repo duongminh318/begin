b99d7a586ae39d8deae155786160158bead2ff08
Win <duongminh318@gmail.com>
2025-09-17 15:26:57 +0700

    lesson13: sử dụng useState để lấy thông tin  nhập vào từ form đăng nhập để xử lý

--- authentication/src/components/Auth.js
+++ authentication/src/components/Auth.js
@@ -1,22 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Auth = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLogin, setIsLogin] = useState("false");
+  const [error, setError] = useState("");
   return (
     <div>
       <h1 className='text-center'>Login Form</h1>
       <form>
         <div className='form-group'>
           <label>Email: </label>
-          <input type="email" className='form-control' />
+          <input type="email" className='form-control'
+            onChange={(event) => { setEmail(event.target.value) }}
+            value={email}
+          />
         </div>
 
         <div className='form-group'>
           <label>Password: </label>
-          <input type="password" className='form-control' />
+          <input type="password" className='form-control'
+            onChange={(event) => { setPassword(event.target.value) }}
+            value={password}
+          />
         </div>
 
         <div className='form-group'>
-          <button type="login">Login</button>
+          <button type="login"
+            className='btn btn-primary'
+          >Login</button>
         </div>
 
       </form>
