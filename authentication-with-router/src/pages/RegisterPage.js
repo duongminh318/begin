7f2952fb3af40bca7c3893b8c3e1bdf659287e43
Win <duongminh318@gmail.com>
2025-09-20 10:56:20 +0700

    lesson14: hoàn thành resgister

--- authentication-with-router/src/pages/RegisterPage.js
+++ authentication-with-router/src/pages/RegisterPage.js
@@ -5,92 +5,92 @@ import { useNavigate } from 'react-router-dom';
 const RegisterPage = () => {
 
 
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const [error, setError] = useState("");
+  const [error, setError] = useState("");
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const register = () => {
+  const register = () => {
 
-        const myHeaders = new Headers();
-        myHeaders.append("Content-Type", "application/json");
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
 
-        const raw = JSON.stringify({
-            "email": email,
-            "password": password
-        });
+    const raw = JSON.stringify({
+      "email": email,
+      "password": password
+    });
 
-        const requestOptions = {
-            method: "POST",
-            headers: myHeaders,
-            body: raw,
-            redirect: "follow"
-        };
+    const requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow"
+    };
 
-        fetch("http://localhost:8000/auth/login", requestOptions)
-            .then((response) => {
-                console.log(response);
-                if (response.ok) {
-                    return response.json();
-                }
+    fetch("http://localhost:8000/auth/register", requestOptions)
+      .then((response) => {
+        console.log(response);
+        if (response.ok) {
+          return response.json();
+        }
 
-                throw new Error(response.statusText)
+        throw new Error(response.statusText)
 
-            })
+      })
 
-            .then((result) => {
-                localStorage.setItem("access_token", result.access_token);
+      .then((result) => {
+        localStorage.setItem("access_token", result.access_token);
 
-                localStorage.setItem("email", email);
-                // redirect to products page
+        localStorage.setItem("email", email);
+        // redirect to products page
 
-                navigate("/products")
-
-
-            })
-            .catch((error) => setError(error.toString()));
-
-    }
+        navigate("/products")
 
 
+      })
+      .catch((error) => setError(error.toString()));
+
+  }
 
 
 
-    return (
-        <div>
 
-            <form style={{ maxWidth: "600px", margin: "auto" }}>
-                <h1 className='text-center'>Register Form</h1>
-                {error ? <div className="alert alert-danger">{error}</div> : ""}
-                <div className='form-group'>
-                    <label>Email: </label>
-                    <input type="email" className='form-control'
-                        onChange={(event) => { setEmail(event.target.value) }}
-                        value={email}
-                    />
-                </div>
 
-                <div className='form-group'>
-                    <label>Password: </label>
-                    <input type="password" className='form-control'
-                        onChange={(event) => { setPassword(event.target.value) }}
-                        value={password}
-                    />
-                </div>
+  return (
+    <div>
 
-                <div className='form-group'>
-                    <button type="button"
-                        className='btn btn-primary'
-                        onClick={() => register()}
-
-                    >Register</button>
-                </div>
-
-            </form>
+      <form style={{ maxWidth: "600px", margin: "auto" }}>
+        <h1 className='text-center'>Register Form</h1>
+        {error ? <div className="alert alert-danger">{error}</div> : ""}
+        <div className='form-group'>
+          <label>Email: </label>
+          <input type="email" className='form-control'
+            onChange={(event) => { setEmail(event.target.value) }}
+            value={email}
+          />
         </div>
-    )
+
+        <div className='form-group'>
+          <label>Password: </label>
+          <input type="password" className='form-control'
+            onChange={(event) => { setPassword(event.target.value) }}
+            value={password}
+          />
+        </div>
+
+        <div className='form-group'>
+          <button type="button"
+            className='btn btn-primary'
+            onClick={() => register()}
+
+          >Register</button>
+        </div>
+
+      </form>
+    </div>
+  )
 }
 
 export default RegisterPage;
