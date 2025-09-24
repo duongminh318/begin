9597e3d2cc48ad3ccf7e37a14f51b8d943505aea
Win <duongminh318@gmail.com>
2025-09-24 16:31:49 +0700

    lesson16: userreducer

--- authentication-saga/src/reducers/userReducer.jsx
+++ authentication-saga/src/reducers/userReducer.jsx
@@ -1,29 +1,39 @@
-import *as ActionType from '../constants/userActionTypes';
+// Import tất cả các hằng số action type từ file userActionTypes
+import * as ActionType from '../constants/userActionTypes';
 
-const initialState= {isLogin:localStorage.getItem("access_token")!=null}
+// Khởi tạo state ban đầu của reducer
+// - isLogin: true nếu trong localStorage có 'access_token', ngược lại false
+const initialState = { 
+  isLogin: localStorage.getItem('access_token') != null 
+};
 
-const usersReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case ActionType.GET_USERS_REQUEST:
-            return {
-                ...state,
-                loading: true,
-            }
-        case ActionType.GET_USERS_SUCCESS:
-            return {
-                ...state,
-                users: action.payload,
-                loading: false,
-            }
-        case ActionType.GET_USERS_FAILED:
-            return {
-                ...state,
-                loading: false,
-                error: action.payload,
-            }
-        default:
-            return state
-    }
+// Khai báo reducer userReducer
+// - state: trạng thái hiện tại (mặc định là initialState)
+// - action: đối tượng mô tả hành động (type + payload)
+const userReducer = (state = initialState, action) => {
+  switch (action.type) {
+
+    // Nếu action là LOGIN_SUCCESS (đăng nhập thành công)
+    case ActionType.LOGIN_SUCCESS:
+      // Cập nhật state: isLogin = true
+      return { isLogin: true };
+
+    // Nếu action là LOGIN_FAILED (đăng nhập thất bại)
+    case ActionType.LOGIN_FAILED:
+      // Cập nhật state: isLogin = false và lưu error từ payload
+      return { isLogin: false, error: action.payload };
+
+    // Nếu action là LOGOUT_SUCCESS (đăng xuất thành công)
+    case ActionType.LOGOUT_SUCCESS:
+      // Cập nhật state: isLogin = false
+      return { isLogin: false };
+
+    // Trường hợp mặc định: không có action nào khớp
+    default:
+      // Giữ nguyên state cũ
+      return state;
+  }
 }
 
-export default usersReducer
+// Xuất reducer để sử dụng trong store Redux
+export default userReducer;
